0aa13d8ea45057786803657bb5a2ec7c96349ff7
IlyaAgarishev <ilyagarishev@gmail.com>
2022-08-09 19:49:02 +0500

    redux-persist removed

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,9 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
-import { persistor, setupStore, store } from "./store/store";
-import { PersistGate } from "redux-persist/integration/react";
+import { store } from "./store/store";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -13,9 +11,7 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <App />
-    </PersistGate>
+    <App />
   </Provider>
 );
 
--- src/store/store.ts
+++ src/store/store.ts
@@ -3,28 +3,17 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import ReduxLogger from "redux-logger";
 import { snakeReducer } from "./reducers/snakeSlice";
-import storage from "redux-persist/lib/storage";
-import { persistReducer, persistStore } from "redux-persist";
 
 const rootReducer = combineReducers({ snakeReducer });
 
-const persistConfig = {
-  key: "root",
-  storage,
-};
-
-const persistedReducer = persistReducer(persistConfig, rootReducer);
-
-export const setupStore = () => {
+const setupStore = () => {
   return configureStore({
-    reducer: persistedReducer,
+    reducer: rootReducer,
     middleware: [ReduxLogger],
   });
 };
 
 export const store = setupStore();
-
-export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof rootReducer>;
 export type AppStore = ReturnType<typeof setupStore>;
