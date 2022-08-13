2dda1263d6059e6934a55641f410388533418eca
IlyaAgarishev <ilyagarishev@gmail.com>
2022-08-14 01:25:19 +0500

    added github link

--- src/components/ContinueScreen/ContinueScreen.tsx
+++ src/components/ContinueScreen/ContinueScreen.tsx
@@ -1,10 +1,19 @@
+import { useEffect } from "react";
 import { Colors } from "../../enums/Colors";
 import { useAppSelector } from "../../hooks/reduxHooks";
+import useOnKeyDown from "../../hooks/useOnKeyDown";
 import styles from "./ContinueScreen.module.css";
 
 const ContinueScreen = () => {
   const score = useAppSelector((state) => state.snakeReducer.score);
   const bestScore = useAppSelector((state) => state.snakeReducer.bestScore);
+  const keyDownData = useOnKeyDown();
+
+  useEffect(() => {
+    if (keyDownData.key === "g") {
+      window.open("https://github.com/IlyaAgarishev/react-gameboy", "_blank");
+    }
+  }, [keyDownData]);
 
   return (
     <div className={styles.continueScreen}>
@@ -17,6 +26,10 @@ const ContinueScreen = () => {
       <div>
         Press <span className={styles.spaceButtonOnScreen}>"Space"</span> to
         start again
+      </div>
+      <div>
+        Press <span className={styles.spaceButtonOnScreen}>"G"</span> to see the
+        <span style={{ color: "#afafaf" }}> github</span> project
       </div>
     </div>
   );
