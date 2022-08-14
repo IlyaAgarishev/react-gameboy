aefea082d1f236b074a0c7c009d80fd463b6a4f8
IlyaAgarishev <ilyagarishev@gmail.com>
2022-08-14 06:10:28 +0500

    removed sound and WelcomeScreen comp

--- src/components/Screen/Screen.tsx
+++ src/components/Screen/Screen.tsx
@@ -1,16 +1,13 @@
-import { useState } from "react";
 import { useAppSelector } from "../../hooks/reduxHooks";
 import useTurnOnGameboy from "../../hooks/useTurnOnGameboy";
 import ContinueScreen from "../ContinueScreen";
 import GameplayScreen from "../GameplayScreen";
-import WelcomeScreen from "../WelcomeScreen";
 import styles from "./Screen.module.css";
 
 const Screen = () => {
   const snakeHasFailed = useAppSelector(
     (state) => state.snakeReducer.snakeHasFailed
   );
-  const [gameIsStarted, setGameIsStarted] = useState(false);
 
   // TODO: попробовать заменить на react-transition-group
   const { batteryIsActivated, screenIsActivated } = useTurnOnGameboy();
@@ -25,13 +22,11 @@ const Screen = () => {
         className={styles.screen}
         style={{ opacity: screenIsActivated ? "1" : "0" }}
       >
-        {screenIsActivated && gameIsStarted ? (
+        {screenIsActivated && (
           <>
             {snakeHasFailed && <ContinueScreen />}
             <GameplayScreen blur={snakeHasFailed} />
           </>
-        ) : (
-          <WelcomeScreen setGameIsStarted={setGameIsStarted} />
         )}
       </div>
     </div>
